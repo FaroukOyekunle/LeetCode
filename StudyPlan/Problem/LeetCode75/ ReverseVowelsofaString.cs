a18325aad0c4df0a8235366af9836a2abfa5f8a8
FaroukOyekunle <oyekunlefarouk2001@gmail.com>
2025-02-12 15:37:36 +0100

    feat: Implement reverse vowels in a string using two-pointer approach
    - Utilized a HashSet for quick lookup of vowels (both uppercase and lowercase).
    - Converted the string to a character array for in-place modifications.
    - Implemented a two-pointer technique:
    - Moved the left pointer forward until it found a vowel.
    - Moved the right pointer backward until it found a vowel.
    - Swapped the vowels at left and right, then moved both pointers inward.
    - Returned the modified string after processing.

--- StudyPlan/Problem/LeetCode75/ ReverseVowelsofaString.cs
+++ StudyPlan/Problem/LeetCode75/ ReverseVowelsofaString.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace StudyPlan.Problem.LeetCode75
 {
     public class ReverseVowelsofaString
