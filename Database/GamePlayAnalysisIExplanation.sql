f2582f701db2efb0d4f52836c2643124df006a31
Fārūqī al-Nūrī <oyekunlefarouk2001@gmail.com>
2025-06-10 13:19:09 +0100

    Added SQL query to retrieve each player's first login date using aggregation.

---
+++ Database/GamePlayAnalysisIExplanation.sql
@@ -0,0 +1,5 @@
+✅ Explanation
+
+-   MIN(event_date): Finds the earliest login date for each player_id.
+
+-   GROUP BY player_id: Ensures we aggregate data per player and get their first login date.
