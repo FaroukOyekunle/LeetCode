be194730df94e64fbfdfbe9252284a38243b10c4
Fārūqī al-Nūrī <oyekunlefarouk2001@gmail.com>
2025-06-13 09:00:44 +0100

    feat: Add query to list salespersons with no orders for company 'RED'

---
+++ Database/SalesPersonExplanation.sql
@@ -0,0 +1,7 @@
+✅ Explanation:
+
+-   I identify the com_id for 'RED' from the Company table.
+
+-   Then find all salespersons who have made orders to that com_id using the Orders table.
+
+-   I then exclude those salespersons from the complete list of salespersons in the SalesPerson table.
