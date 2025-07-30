47bfafa7fa492713a6612f25f89362765200c0a7
Fārūqī al-Nūrī <oyekunlefarouk2001@gmail.com>
2025-07-30 22:45:35 +0100

    feat: Pivot Department revenue by month using CASE and GROUP BY

--- Database/ReformatDepartmentTableQuestion.sql
+++ Database/ReformatDepartmentTableQuestion.sql
@@ -11,7 +11,6 @@ In SQL,(id, month) is the primary key of this table.
 The table has information about the revenue of each department per month.
 The month has values in ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"].
  
-
 Reformat the table such that there is a department id column and a revenue column for each month.
 
 Return the result table in any order.
