a3c70700eb528061aa441c69f676144fb052166c
Fārūqī al-Nūrī <oyekunlefarouk2001@gmail.com>
2025-09-07 20:17:02 +0100

    feat: Add SQL query to fetch users with balance > 10000 by joining Users and Transactions

--- Database/BankAccountSummaryIIQuestion.sql
+++ Database/BankAccountSummaryIIQuestion.sql
@@ -25,7 +25,6 @@ trans_id is the primary key (column with unique values) for this table.
 Each row of this table contains all changes made to all accounts.
 amount is positive if the user received money and negative if they transferred money.
 All accounts start with a balance of 0.
- 
 
 Write a solution to report the name and balance of users with a balance higher than 10000. The balance of an account is equal to the sum of the amounts of all transactions involving that account.
 
