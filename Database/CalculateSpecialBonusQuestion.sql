2c4e56056f1211fcb916b8e468a2bfb4019241fa
Fārūqī al-Nūrī <oyekunlefarouk2001@gmail.com>
2025-09-09 12:59:50 +0100

    Calculate employee bonus based on ID and name conditions
    - Employees with odd IDs and names not starting with 'M' get 100% salary as bonus.
    - All other employees receive a 0 bonus.
    - Results are ordered by employee_id.

--- Database/CalculateSpecialBonusQuestion.sql
+++ Database/CalculateSpecialBonusQuestion.sql
@@ -10,7 +10,6 @@ Table: Employees
 employee_id is the primary key (column with unique values) for this table.
 Each row of this table indicates the employee ID, employee name, and salary.
  
-
 Write a solution to calculate the bonus of each employee. The bonus of an employee is 100% of their salary if the ID of the employee is an odd number and the employee''s 
 name does not start with the character 'M'. The bonus of an employee is 0 otherwise.
 
