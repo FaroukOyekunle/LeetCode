e49648860368b7d18a963c37cd64ed516a9bdf2b
Fārūqī al-Nūrī <oyekunlefarouk2001@gmail.com>
2025-09-11 08:24:35 +0100

    fix: correctly compute capital gain/loss by pairing each Buy with the corresponding Sell using row numbers

--- Database/CapitalGainLossQuestion.sql
+++ Database/CapitalGainLossQuestion.sql
@@ -13,7 +13,6 @@ The operation column is an ENUM (category) of type ('Sell', 'Buy')
 Each row of this table indicates that the stock which has stock_name had an operation on the day operation_day with the price.
 It is guaranteed that each 'Sell' operation for a stock has a corresponding 'Buy' operation in a previous day. It is also guaranteed that each 'Buy' operation for a stock has a corresponding 'Sell' operation in an upcoming day.
  
-
 Write a solution to report the Capital gain/loss for each stock.
 
 The Capital gain/loss of a stock is the total gain or loss after buying and selling the stock one or many times.
