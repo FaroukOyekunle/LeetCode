eb61264ebb61c55ce9ff1e1ca6c6b7701a332ee3
Fārūqī al-Nūrī <oyekunlefarouk2001@gmail.com>
2026-03-27 15:27:38 +0100

    Optimize matrix similarity check using cyclic shift modulo
    - Avoid full simulation of k operations
    - Use k % columnCount to compute effective shift
    - Compare elements in-place based on row parity:
      - Even rows: left cyclic shift
      - Odd rows: right cyclic shift
    - Achieve O(m * n) time and O(1) space complexity

---
+++ DailyQuestion/MatrixSimilarityAfterCyclicShifts.cs
@@ -0,0 +1,41 @@
+namespace DailyQuestion
+{
+    public class MatrixSimilarityAfterCyclicShifts
+    {
+        public bool AreRowsInvariantUnderAlternatingShifts(int[][] inputMatrix, int shiftDistance)
+        {
+            int totalRowCount = inputMatrix.Length;
+            int totalColumnCount = inputMatrix[0].Length;
+
+            int normalizedShiftDistance = shiftDistance % totalColumnCount;
+
+            for (int currentRowIndex = 0; currentRowIndex < totalRowCount; currentRowIndex++)
+            {
+                for (int currentColumnIndex = 0; currentColumnIndex < totalColumnCount; currentColumnIndex++)
+                {
+                    int correspondingShiftedColumnIndex;
+
+                    if (currentRowIndex % 2 == 0)
+                    {
+                        correspondingShiftedColumnIndex =
+                            (currentColumnIndex + normalizedShiftDistance) % totalColumnCount;
+                    }
+
+                    else
+                    {
+                        correspondingShiftedColumnIndex =
+                            (currentColumnIndex - normalizedShiftDistance + totalColumnCount) % totalColumnCount;
+                    }
+
+                    if (inputMatrix[currentRowIndex][currentColumnIndex] !=
+                        inputMatrix[currentRowIndex][correspondingShiftedColumnIndex])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
