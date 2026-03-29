258ba70b6e92dd95efbf628f4aef5b6655fc1071
Fārūqī al-Nūrī <oyekunlefarouk2001@gmail.com>
2026-03-29 16:11:49 +0100

    feat: determine if two strings can be made equal via index+2 swaps
    - Identified invariant: indices split into even (0,2) and odd (1,3) groups
    - Sorted and compared both groups independently
    - Ensures correctness with O(1) time and space complexity

--- DailyQuestion/CheckifStringsCanbeMadeEqualWithOperationsI.cs
+++ DailyQuestion/CheckifStringsCanbeMadeEqualWithOperationsI.cs
@@ -4,17 +4,13 @@ namespace DailyQuestion
     {
         public bool CanMakeStringsEqualUsingIndexDistanceTwoSwaps(string firstString, string secondString)
         {
-            char[] firstStringEvenIndexedCharacters =
-                new char[] { firstString[0], firstString[2] };
+            char[] firstStringEvenIndexedCharacters = new char[] { firstString[0], firstString[2] };
 
-            char[] firstStringOddIndexedCharacters =
-                new char[] { firstString[1], firstString[3] };
+            char[] firstStringOddIndexedCharacters = new char[] { firstString[1], firstString[3] };
 
-            char[] secondStringEvenIndexedCharacters =
-                new char[] { secondString[0], secondString[2] };
+            char[] secondStringEvenIndexedCharacters = new char[] { secondString[0], secondString[2] };
 
-            char[] secondStringOddIndexedCharacters =
-                new char[] { secondString[1], secondString[3] };
+            char[] secondStringOddIndexedCharacters =  new char[] { secondString[1], secondString[3] };
 
             Array.Sort(firstStringEvenIndexedCharacters);
             Array.Sort(firstStringOddIndexedCharacters);
